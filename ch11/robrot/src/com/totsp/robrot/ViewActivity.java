05c5ef71af8b21ac0740616591cf8eac82e5dd50
kebernet <kebernet@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-06-10 16:26:09 +0000

    Initial

---
+++ ch11/robrot/src/com/totsp/robrot/ViewActivity.java
@@ -0,0 +1,46 @@
+package com.totsp.robrot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ViewActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	private MandelbrotView brot = null;
+	private Button reset = null;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        this.brot = (MandelbrotView) this.findViewById(R.id.brot);
+        this.reset = (Button) this.findViewById(R.id.reset);
+        this.reset.setOnClickListener( new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				brot.reset();
+			}
+        	
+        });
+    }
+    
+    @Override
+    protected void onPause(){
+    	if(brot != null){
+    		brot.cancel();
+    	}
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onResume(){
+    	if(brot != null){
+    		brot.start();
+    	}
+    	super.onResume();
+    }
+}
