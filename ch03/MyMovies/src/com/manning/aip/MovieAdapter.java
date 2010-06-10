f640e74ce7129a7febe57b456850ba7b710cd58b
m.kaeppler <m.kaeppler@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-06-10 18:50:48 +0000

    apply AIP formatting and cleanup

--- ch03/MyMovies/src/com/manning/aip/MovieAdapter.java
+++ ch03/MyMovies/src/com/manning/aip/MovieAdapter.java
@@ -10,38 +10,40 @@ import android.widget.CheckedTextView;
 
 public class MovieAdapter extends ArrayAdapter<String> {
 
-    private HashMap<Integer, Boolean> movieCollection = new HashMap<Integer, Boolean>();
+   private HashMap<Integer, Boolean> movieCollection =
+            new HashMap<Integer, Boolean>();
 
-    public MovieAdapter(Context context) {
-        super(context, R.layout.movie_item, android.R.id.text1,
-            context.getResources().getStringArray(R.array.movies));
-    }
+   public MovieAdapter(Context context) {
+      super(context, R.layout.movie_item, android.R.id.text1, context
+               .getResources().getStringArray(R.array.movies));
+   }
 
-    public void toggleMovie(int position) {
-        if (!isInCollection(position)) {
-            movieCollection.put(position, true);
-        } else {
-            movieCollection.put(position, false);
-        }
-    }
+   public void toggleMovie(int position) {
+      if (!isInCollection(position)) {
+         movieCollection.put(position, true);
+      } else {
+         movieCollection.put(position, false);
+      }
+   }
 
-    public boolean isInCollection(int position) {
-        return movieCollection.get(position) == Boolean.TRUE;
-    }
+   public boolean isInCollection(int position) {
+      return movieCollection.get(position) == Boolean.TRUE;
+   }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+   @Override
+   public View getView(int position, View convertView, ViewGroup parent) {
 
-        // View listItem = inflater.inflate(R.layout.movie_item, null);
-        View listItem = super.getView(position, convertView, parent);
+      // View listItem = inflater.inflate(R.layout.movie_item, null);
+      View listItem = super.getView(position, convertView, parent);
 
-        CheckedTextView checkMark = (CheckedTextView) listItem.findViewById(android.R.id.text1);
-        checkMark.setChecked(isInCollection(position));
+      CheckedTextView checkMark =
+               (CheckedTextView) listItem.findViewById(android.R.id.text1);
+      checkMark.setChecked(isInCollection(position));
 
-        // TextView movieRank = (TextView)
-        // listItem.findViewById(R.id.movie_rank);
-        // movieRank.setText(Integer.toString(position + 1));
+      // TextView movieRank = (TextView)
+      // listItem.findViewById(R.id.movie_rank);
+      // movieRank.setText(Integer.toString(position + 1));
 
-        return listItem;
-    }
+      return listItem;
+   }
 }
--- ch03/MyMovies/src/com/manning/aip/MyMovies.java
+++ ch03/MyMovies/src/com/manning/aip/MyMovies.java
@@ -8,31 +8,33 @@ import android.widget.ListView;
 
 public class MyMovies extends ListActivity {
 
-    private MovieAdapter adapter;
+   private MovieAdapter adapter;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main);
+      setContentView(R.layout.main);
 
-        ListView listView = getListView();
+      ListView listView = getListView();
 
-        Button backToTop = (Button) getLayoutInflater().inflate(R.layout.list_footer, null);
-        backToTop.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(
-                android.R.drawable.ic_menu_upload), null, null, null);
-        listView.addFooterView(backToTop, null, true);
+      Button backToTop =
+               (Button) getLayoutInflater().inflate(R.layout.list_footer, null);
+      backToTop.setCompoundDrawablesWithIntrinsicBounds(getResources()
+               .getDrawable(android.R.drawable.ic_menu_upload), null, null,
+               null);
+      listView.addFooterView(backToTop, null, true);
 
-        this.adapter = new MovieAdapter(this);
-        listView.setAdapter(this.adapter);
-        listView.setItemsCanFocus(false);
-    }
+      this.adapter = new MovieAdapter(this);
+      listView.setAdapter(this.adapter);
+      listView.setItemsCanFocus(false);
+   }
 
-    public void backToTop(View view) {
-        getListView().setSelection(0);
-    }
+   public void backToTop(View view) {
+      getListView().setSelection(0);
+   }
 
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        this.adapter.toggleMovie(position);
-        this.adapter.notifyDataSetChanged();
-    }
+   protected void onListItemClick(ListView l, View v, int position, long id) {
+      this.adapter.toggleMovie(position);
+      this.adapter.notifyDataSetChanged();
+   }
 }
