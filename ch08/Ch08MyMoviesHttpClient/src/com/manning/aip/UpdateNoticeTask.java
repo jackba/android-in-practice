00e031590e3a51a36c6dbbe254bef8bf35ce2d1d
m.kaeppler <m.kaeppler@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-08-10 13:41:50 +0000

    customize http client

--- ch08/Ch08MyMoviesHttpClient/src/com/manning/aip/UpdateNoticeTask.java
+++ ch08/Ch08MyMoviesHttpClient/src/com/manning/aip/UpdateNoticeTask.java
@@ -1,11 +1,20 @@
 package com.manning.aip;
 
-import java.net.HttpURLConnection;
-
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 
 import android.os.AsyncTask;
@@ -13,24 +22,50 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
-public class UpdateNoticeTask extends AsyncTask<String, Void, String> {
+public class UpdateNoticeTask extends AsyncTask<Void, Void, String> {
+
+   private String updateUrl =
+            "http://android-in-practice.googlecode.com/files/update_notice.txt";
 
    private Handler handler;
 
-   private HttpClient httpClient = new DefaultHttpClient();
+   private HttpClient httpClient;
 
    public UpdateNoticeTask(Handler handler) {
       this.handler = handler;
+      setupHttpClient();
+   }
+
+   private void setupHttpClient() {
+      SchemeRegistry schemeRegistry = new SchemeRegistry();
+      schemeRegistry.register(new Scheme("http", PlainSocketFactory
+               .getSocketFactory(), 80));
+
+      HttpParams connManagerParams = new BasicHttpParams();
+      ConnManagerParams.setMaxTotalConnections(connManagerParams, 5);
+      ConnManagerParams.setMaxConnectionsPerRoute(connManagerParams,
+               new ConnPerRouteBean(5));
+      ConnManagerParams.setTimeout(connManagerParams, 15 * 1000);
+
+      ThreadSafeClientConnManager cm =
+               new ThreadSafeClientConnManager(connManagerParams,
+                        schemeRegistry);
+
+      HttpParams clientParams = new BasicHttpParams();
+      HttpProtocolParams.setUserAgent(clientParams, "MyMovies/1.0");
+      HttpConnectionParams.setConnectionTimeout(clientParams, 15 * 1000);
+      HttpConnectionParams.setSoTimeout(clientParams, 15 * 1000);
+      httpClient = new DefaultHttpClient(cm, clientParams);
    }
 
    @Override
-   protected String doInBackground(String... params) {
+   protected String doInBackground(Void... params) {
       try {
-         HttpGet request = new HttpGet(params[0]);
+         HttpGet request = new HttpGet(updateUrl);
          request.setHeader("Accept", "text/plain");
          HttpResponse response = httpClient.execute(request);
          int statusCode = response.getStatusLine().getStatusCode();
-         if (statusCode != HttpURLConnection.HTTP_OK) {
+         if (statusCode != HttpStatus.SC_OK) {
             return "Error: Failed getting update notes";
          }
          return EntityUtils.toString(response.getEntity());
