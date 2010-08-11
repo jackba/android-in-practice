bedfca8df2bd80660eef7050260e88a5caacd870
m.kaeppler <m.kaeppler@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-08-11 08:58:45 +0000

    getting rid of the boolean state flags

--- ch08/Ch08MyMoviesHttpClient/src/com/manning/aip/xml/SAXMovieParser.java
+++ ch08/Ch08MyMoviesHttpClient/src/com/manning/aip/xml/SAXMovieParser.java
@@ -6,7 +6,6 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import android.text.TextUtils;
 import android.util.Xml;
 import android.util.Xml.Encoding;
 
@@ -14,9 +13,9 @@ import com.manning.aip.Movie;
 
 public class SAXMovieParser extends DefaultHandler {
 
-   private Movie movie = new Movie();
+   private Movie movie;
 
-   private boolean parseMovie, parseName, parseRating;
+   private StringBuilder elementText;
 
    public static Movie parseMovie(InputStream xml) throws Exception {
       SAXMovieParser parser = new SAXMovieParser();
@@ -29,45 +28,32 @@ public class SAXMovieParser extends DefaultHandler {
    }
 
    @Override
+   public void startDocument() throws SAXException {
+      elementText = new StringBuilder();
+   }
+
+   @Override
    public void startElement(String uri, String localName, String qName,
             Attributes attributes) throws SAXException {
       if ("movie".equals(localName)) {
-         parseMovie = true;
-      } else if ("name".equals(localName)) {
-         parseName = true;
-      } else if ("rating".equals(localName)) {
-         parseRating = true;
+         movie = new Movie();
       }
    }
 
    @Override
    public void endElement(String uri, String localName, String qName)
       throws SAXException {
-      if ("movie".equals(localName)) {
-         parseMovie = false;
-      } else if ("name".equals(localName)) {
-         parseName = false;
+
+      if ("name".equals(localName)) {
+         movie.setTitle(elementText.toString().trim());
       } else if ("rating".equals(localName)) {
-         parseRating = false;
+         movie.setRating(elementText.toString().trim());
       }
+      elementText.setLength(0);
    }
 
    @Override
    public void characters(char[] ch, int start, int length) throws SAXException {
-      if (!parseMovie) {
-         return;
-      }
-      String text = new String(ch, start, length).trim();
-      if (TextUtils.isEmpty(text)) {
-         return;
-      }
-
-      if (parseName) {
-         movie.setTitle(text);
-         parseName = false;
-      } else if (parseRating) {
-         movie.setRating(text);
-         parseRating = false;
-      }
+      elementText.append(ch, start, length);
    }
 }
