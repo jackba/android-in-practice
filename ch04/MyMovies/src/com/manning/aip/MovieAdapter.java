580e04dee5a0411d2097055ee6e91e372c841aab
charlie.collins <charlie.collins@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-09-18 21:48:06 +0000

    move to chapter 4

---
+++ ch04/MyMovies/src/com/manning/aip/MovieAdapter.java
@@ -0,0 +1,49 @@
+package com.manning.aip;
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+
+public class MovieAdapter extends ArrayAdapter<String> {
+
+   private HashMap<Integer, Boolean> movieCollection =
+            new HashMap<Integer, Boolean>();
+
+   public MovieAdapter(Context context) {
+      super(context, R.layout.movie_item, android.R.id.text1, context
+               .getResources().getStringArray(R.array.movies));
+   }
+
+   public void toggleMovie(int position) {
+      if (!isInCollection(position)) {
+         movieCollection.put(position, true);
+      } else {
+         movieCollection.put(position, false);
+      }
+   }
+
+   public boolean isInCollection(int position) {
+      return movieCollection.get(position) == Boolean.TRUE;
+   }
+
+   @Override
+   public View getView(int position, View convertView, ViewGroup parent) {
+
+      // View listItem = inflater.inflate(R.layout.movie_item, null);
+      View listItem = super.getView(position, convertView, parent);
+
+      CheckedTextView checkMark =
+               (CheckedTextView) listItem.findViewById(android.R.id.text1);
+      checkMark.setChecked(isInCollection(position));
+
+      // TextView movieRank = (TextView)
+      // listItem.findViewById(R.id.movie_rank);
+      // movieRank.setText(Integer.toString(position + 1));
+
+      return listItem;
+   }
+}
