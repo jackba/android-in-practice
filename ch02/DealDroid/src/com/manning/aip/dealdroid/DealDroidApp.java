2f39daba737586358a5283e8dce73733aa5f9570
charlie.collins <charlie.collins@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-12-01 01:13:19 +0000

    set timeouts on conns

--- ch02/DealDroid/src/com/manning/aip/dealdroid/DealDroidApp.java
+++ ch02/DealDroid/src/com/manning/aip/dealdroid/DealDroidApp.java
@@ -17,6 +17,7 @@ import com.manning.aip.dealdroid.xml.DailyDealsXmlPullFeedParser;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -79,7 +80,14 @@ public class DealDroidApp extends Application {
       Bitmap bitmap = null;
       try {
          URL url = new URL(urlString);
-         bitmap = BitmapFactory.decodeStream(url.openStream());
+         // NOTE, be careful about just doing "url.openStream()"  
+         // it's a shortcut for openConnection().getInputStream() and doesn't set timeouts
+         // (the defaults are "infinite" so it will wait forever if endpoint server is down)
+         // do it properly with a few more lines of code . . .         
+         URLConnection conn = url.openConnection();     
+         conn.setConnectTimeout(3000);
+         conn.setReadTimeout(5000);         
+         bitmap = BitmapFactory.decodeStream(conn.getInputStream());
       } catch (MalformedURLException e) {
          Log.e(Constants.LOG_TAG, "Exception loading image, malformed URL", e);
       } catch (IOException e) {
