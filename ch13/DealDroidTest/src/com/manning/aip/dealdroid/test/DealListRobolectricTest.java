4524273c547da2970845a7e9f26631b5c89c056a
m.kaeppler <m.kaeppler@a0ebf94f-5f61-d500-1403-30a3c231a175>
2010-12-19 16:13:14 +0000

    remove Robotium from this project; add Robolectric

---
+++ ch13/DealDroidTest/src/com/manning/aip/dealdroid/test/DealListRobolectricTest.java
@@ -0,0 +1,17 @@
+package com.manning.aip.dealdroid.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class DealListRobolectricTest {
+
+   @Test
+   public void testTruth() {
+      Assert.assertTrue(true);
+   }
+
+}
