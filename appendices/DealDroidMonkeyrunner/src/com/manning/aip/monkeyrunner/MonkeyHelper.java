8b3e691cbf3d5e63c18917b2246c4924b0536ec1
m.kaeppler <m.kaeppler@a0ebf94f-5f61-d500-1403-30a3c231a175>
2011-08-15 20:14:42 +0000

    fix MonkeyHelper to work with the latest SDK tools release

--- appendices/DealDroidMonkeyrunner/src/com/manning/aip/monkeyrunner/MonkeyHelper.java
+++ appendices/DealDroidMonkeyrunner/src/com/manning/aip/monkeyrunner/MonkeyHelper.java
@@ -1,18 +1,26 @@
 package com.manning.aip.monkeyrunner;
 
+import org.python.core.PyInteger;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+
 import com.android.monkeyrunner.MonkeyDevice;
-import com.android.monkeyrunner.MonkeyDevice.TouchPressType;
+import com.android.monkeyrunner.core.TouchPressType;
 
 public class MonkeyHelper {
 
     public static void tap(MonkeyDevice device, int x, int y) {
-        device.touch(x, y, TouchPressType.DOWN_AND_UP);
+        PyObject[] args = { new PyInteger(x), new PyInteger(y),
+                new PyString(TouchPressType.DOWN_AND_UP.name()) };
+        device.touch(args, null);
         sleep(2000);
     }
 
     public static void press(MonkeyDevice device, String key) {
         String keyCode = "KEYCODE_" + key.toUpperCase();
-        device.press(keyCode, TouchPressType.DOWN_AND_UP);
+        PyObject[] args = { new PyString(keyCode), new PyString(TouchPressType.DOWN_AND_UP.name()),
+                new PyString("") };
+        device.press(args, null);
         sleep(2000);
     }
 
